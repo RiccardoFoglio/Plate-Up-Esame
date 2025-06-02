88f77bb055785fce19cdf3c7798b50afebce4f2e
GIUSEPPE DI STEFANO s337626 <s337626@studenti.polito.it>
2025-06-02 11:02:54 +0200

    Recepie debug

--- Recipe.h
+++ Recipe.h
@@ -3,6 +3,7 @@
 #define RECIPE_H
 
 #include <string>
+#include <vector>
 #include "inventory.h"
 
 class Recipe {
@@ -22,11 +23,29 @@ public:
         : name(recipeName), pane(p), carne(c), formaggio(f), insalata(i), pomodori(pom), uovo(u) {
     }
 
-    bool isSatisfiedBy(const Inventory& inv) const;
+    bool isSatisfiedBy(Inventory& inv) const;
 
     const std::string& getName() const { return name; }
 
     static Recipe getRandomRecipe(int level);
+
+    const std::vector<std::string>& getCurrentIngredients() const {
+        static std::vector<std::string> currentIngredients;
+        currentIngredients.clear();
+    if (pane) currentIngredients.push_back("Pane");
+    if (carne) currentIngredients.push_back("Carne");
+    if (formaggio) currentIngredients.push_back("Formaggio");
+    if (insalata) currentIngredients.push_back("Insalata");
+    if (pomodori) currentIngredients.push_back("Pomodori");
+    if (uovo) currentIngredients.push_back("Uovo");
+	if (currentIngredients.empty()) {
+		currentIngredients.push_back("Nessun ingrediente");
+	}
+
+
+    return currentIngredients;
+    }
+
 };
 
 #endif // RECIPE_H
